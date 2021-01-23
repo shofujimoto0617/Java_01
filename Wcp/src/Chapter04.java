bc447007f6755b8f01355c5c0202e3ceb5c4d668
shofujimoto0617 <shofujimoto017@gmail.com>
2021-01-23 15:57:26 +0900

    Chapter04

---
+++ Wcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Sho";
+		System.out.println(name);
+
+		name = "Fujimoto";
+		System.out.println(name);
+
+		int num = 777;
+		System.out.println(num);
+	}
+}
